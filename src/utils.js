88436866c4bfc3e464098941659864290eb23027
Mike4610 <mike22vieiraa@gmail.com>
2021-03-18 15:32:50 +0000

    Removed file from repository

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  fileName: function (db) {
-    return (db === "" || db === undefined) ? "index.json" : (db.endsWith('.json') ? db : db + '.json')
-  },
-  setOptions: function(options){
-    return (options === undefined) ? {pretty: false, id: false, schema: false} : options
-  }
-};
